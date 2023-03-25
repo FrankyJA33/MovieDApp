4364c6384414c3e4423c0137f4bcfb5c0ce3a353
Francisco Leon <pac6831@hotmail.com>
2023-03-24 20:01:45 -0600

    Migracion a Axios<3

--- src/main.js
+++ src/main.js
@@ -1,24 +1,30 @@
 const sectionTradingPreview = document.querySelector('#trendingPreview .trendingPreview-movieList');
 const sectionCategoriesPreview = document.querySelector('#categoriesPreview .categoriesPreview-list');
 
-const getTrendingMoviePreview = async () => {
-    const response = await fetch(`https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}`);
-    const data = await response.json();
+const api = axios.create({
+    baseURL: 'https://api.themoviedb.org/3',
+    headers: {
+        'Content-Type': 'application/json;charset=utf8',
+    },
+    params: {
+        'api_key': API_KEY,
+    },
+});
 
+const getTrendingMoviePreview = async () => {
+    const { data } = await api.get('/trending/movie/day');
     const movies = data.results;
     movies.forEach(element => {
         sectionTradingPreview.innerHTML += `
-        <div class="movie-container">
-            <img src="https://image.tmdb.org/t/p/w300${element.poster_path}" alt="${element.title}" class="movie-img">
-        </div>
+            <div class="movie-container">
+                <img src="https://image.tmdb.org/t/p/w300${element.poster_path}" alt="${element.title}" class="movie-img">
+            </div>
         `;
     });
 }
 
 const getCategoriesPreview = async () => {
-    const response = await fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`);
-    const data = await response.json();
-
+    const { data, status } = await api.get('/genre/movie/list');
     const categories = data.genres;
     categories.forEach(element => {
         sectionCategoriesPreview.innerHTML += `
