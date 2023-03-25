b8f9f6782e40601fcc0b54b92d3c8bce207348a3
Francisco Leon <pac6831@hotmail.com>
2023-03-24 19:23:06 -0600

    Lista de peliculas en tendencia usando ForEach

--- src/main.js
+++ src/main.js
@@ -1,15 +1,16 @@
-const sectionTradingPreview = document.getElementById('trendingPreview-movieList');
+const sectionTradingPreview = document.querySelector('#trendingPreview .trendingPreview-movieList');
 
 const getTrendingMoviePreview = async () => {
     const response = await fetch(`https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}`);
     const data = await response.json();
 
     const movies = data.results;
-    let movieTrandingPreview = `${movies.map(element => `
+    movies.forEach(element => {
+        sectionTradingPreview.innerHTML += `
         <div class="movie-container">
             <img src="https://image.tmdb.org/t/p/w300${element.poster_path}" alt="${element.title}" class="movie-img">
         </div>
-    `)}`;
-    sectionTradingPreview.innerHTML = movieTrandingPreview;
+        `;
+    });
 }
 getTrendingMoviePreview();
