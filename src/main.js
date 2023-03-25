934efc2493b7855d33c78d216dd5f1628686886c
Francisco Leon <pac6831@hotmail.com>
2023-03-24 19:05:43 -0600

    Lista de peliculas en tendencia

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
-console.log("Hola mundo");
+const sectionTradingPreview = document.getElementById('trendingPreview-movieList');
+
+const getTrendingMoviePreview = async () => {
+    const response = await fetch(`https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}`);
+    const data = await response.json();
+
+    const movies = data.results;
+    let movieTrandingPreview = `${movies.map(element => `
+        <div class="movie-container">
+            <img src="https://image.tmdb.org/t/p/w300${element.poster_path}" alt="${element.title}" class="movie-img">
+        </div>
+    `)}`;
+    sectionTradingPreview.innerHTML = movieTrandingPreview;
+}
+getTrendingMoviePreview();
