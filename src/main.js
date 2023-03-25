f81400e868cbe13b87f8969feba4f9bfdece2938
Francisco Leon <pac6831@hotmail.com>
2023-03-24 19:40:26 -0600

    Lista de categorias

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 const sectionTradingPreview = document.querySelector('#trendingPreview .trendingPreview-movieList');
+const sectionCategoriesPreview = document.querySelector('#categoriesPreview .categoriesPreview-list');
 
 const getTrendingMoviePreview = async () => {
     const response = await fetch(`https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}`);
@@ -13,4 +14,19 @@ const getTrendingMoviePreview = async () => {
         `;
     });
 }
+
+const getCategoriesPreview = async () => {
+    const response = await fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`);
+    const data = await response.json();
+
+    const categories = data.genres;
+    categories.forEach(element => {
+        sectionCategoriesPreview.innerHTML += `
+            <div class="category-container">
+                <h3 id="id${element.id}" class="category-title">${element.name}</h3>
+            </div>
+        `;
+    });
+}
 getTrendingMoviePreview();
+getCategoriesPreview();
